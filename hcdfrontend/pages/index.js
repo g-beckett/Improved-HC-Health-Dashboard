c315ea48d4c31889644b22fe3443d6f75dc2f8cc
bradymo64 <90003219+bradymo64@users.noreply.github.com>
2024-02-10 15:57:13 -0500

    homepage setup for testing
    not finished

---
+++ hcdfrontend/pages/index.js
@@ -0,0 +1,21 @@
+import styles from './_app.js';
+import Head from 'next/head';
+import React, { useState, useEffect } from 'react';
+
+
+export default function Home() {
+  
+  /** PAGE CONTENT */
+  
+  return (
+    <div className="relative h-screen">
+      <Head>
+        <title>Hamilton County Health Dashboard</title>
+        <link rel="icon" />
+      </Head>
+      <div className='flex-center text-4xl'>Testing Testing</div>
+
+      </div>
+
+  );
+}
