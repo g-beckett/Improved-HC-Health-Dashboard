c6e0d41e578ce6f7c6b0feba8284e81cf9ea08d0
codydev901 <codydev901@gmail.com>
2024-02-13 07:50:04 -0500

    template inheritence, stylesheet, some basic css

--- hcdbackend/dataportal/urls.py
+++ hcdbackend/dataportal/urls.py
@@ -6,10 +6,10 @@ app_name = 'dataportal'
 urlpatterns = [
     path("", views.index, name="index"),
     path("disease_category", views.disease_category_all, name="disease_category_all"),
-    path("disease_category/<int:pk>", views.disease_category_all, name="disease_category_detail"),
-    path("disease", views.disease_category_all, name="disease_all"),
-    path("disease/<int:pk>", views.disease_category_all, name="disease_detail"),
-    path("case_report", views.disease_category_all, name="case_report_all"),
-    path("case_report/<int:pk>", views.disease_category_all, name="case_report_detail")
+    path("disease_category/<int:pk>", views.disease_category_detail, name="disease_category_detail"),
+    path("disease", views.disease_all, name="disease_all"),
+    path("disease/<int:pk>", views.disease_detail, name="disease_detail"),
+    path("case_report", views.case_report_all, name="case_report_all"),
+    path("case_report/<int:pk>", views.case_report_detail, name="case_report_detail")
 ]
 
